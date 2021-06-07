bc89be1fe2ef5de2b36727e592681069c9ea1ff9
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2021-06-06 21:36:57 -0700

    Restructure.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name='autosat',
+    version='0.1',
+    packages=['autosat'],
+    license='CC0',
+    long_description="Library for making SAT instances",
+)
+
